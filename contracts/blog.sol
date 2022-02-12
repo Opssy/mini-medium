be634317b5bf32cfc437c72b75f95616499515b7
unicode04 <opeyemi.olugbade@kominiti.net>
2022-02-12 16:45:29 +0100

    codebase updated

---
+++ contracts/blog.sol
@@ -0,0 +1,102 @@
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Blog {
+  string public name;
+  address public owner;
+
+  using Counters for Counters.Counter;
+  Counters.Counter private _postIds;
+
+  struct Post{
+    uint id;
+    string title;
+    string content;
+    uint date;
+    bool published;
+  }
+  /* here we create lookups for posts by id and posts by ipfs hash */
+ mapping (uint => Post) private idToPost;
+ mapping (string => Post) private hashToPost;
+
+ event PostCreated(uint id, string title, string hash);
+ event PostUpdated(uint id, string title, string hash, bool published);
+
+ /* when the blog is deployed, give it a name */
+     /* also set the creator as the owner of the contract */
+ constructor(string memory _name) {
+   console.log("Deploying Blog with name:", _name);
+   name = _name;
+   owner = msg.sender;
+ }
+
+ /* updates the blog name */
+function updateName(string memory _name) public {
+  name = _name;
+}
+
+ /* transfers ownership of the contract to another address */
+ function transferOwnership(address newOwner ) public onlyOwner {
+   owner = newOwner;
+ }
+
+ //fetch post by individual post
+ function fetchPost(string memory hash) public view returns(Post memory){
+     return hashToPost[hash];
+   }
+ //fetch post by date
+
+ //Create a post
+ function createPost(string memory title, string memory hash) public  onlyOwner {
+   _postIds.increment();
+  uint postId = _postIds.current();
+   Post storage post = idToPost[postId];
+   post.id = postId;
+   post.title = title;
+   post.published = true;
+   //date
+   post.content = hash;
+   hashToPost[hash] = post;
+   emit PostCreated(postId, title, hash);
+ }
+
+ //tO UPDATE EXISTING Code
+ function updatePost(uint postId, string memory title, string memory hash, bool published) public onlyOwner {
+     Post storage post =  idToPost[postId];
+     post.title = title;
+     post.published = published;
+     post.content = hash;
+     idToPost[postId] = post;
+     hashToPost[hash] = post;
+     emit PostUpdated(post.id, title, hash, published);
+ }
+
+ //to fetch all post
+ function fetchPosts() public view returns (Post [] memory) {
+   uint itemCount = _postIds.current();
+   uint currentIndex = 0;
+   Post[] memory posts = new Post[](itemCount);
+   for (uint i = 0; i < itemCount; i++) {
+     uint currentId = i + 1;
+     Post storage currentItem = idToPost[currentId];
+     posts[currentIndex] = currentItem;
+     currentIndex +=1;
+   }
+   return posts;
+ }
+ //only the owner of the contract can modify the contracts
+ modifier onlyOwner() {
+   require(msg.sender == owner);
+   _;
+ }
+}
+
+
+//short note
+/*
+This contract allows the owner to create and edit posts, and for anyone to fetch posts.
+To make this smart contract permissionless, you could remove the onlyOwner modifier and use
+The Graph to index and query posts by owner.
+*/
---
+++ scripts/deploy.js
@@ -0,0 +1,30 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+const fs = require('fs');
+
+async function main() {
+  /* these two lines deploy the contract to the network */
+  const Blog = await hre.ethers.getContractFactory("Blog");
+  const blog = await Blog.deploy("My blog");
+
+  await blog.deployed();
+  console.log("Blog deployed to:", blog.address);
+
+  /* this code writes the contract addresses to a local */
+  /* file named config.js that we can use in the app */
+  fs.writeFileSync('./config.js', `
+  export const contractAddress = "${blog.address}"
+  export const ownerAddress = "${blog.signer.address}"
+  `)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
